c0d9d8ce2fd1b511663c0a04a586b39fae60efb1
Sabithra <sabithra777@gmail.com>
2025-12-02 11:28:40 +0530

    Initial commit for Day 2 work

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import HelloWorldComponent from "./hello-world/hello-world-component";
+
+function App() {
+  return (
+    <HelloWorldComponent/>
+  );
+}
+
+export default App;
