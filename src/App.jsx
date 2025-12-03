3c2776272dfabc1ef5a2731256ee6840b14db99d
Sabithra <sabithra777@gmail.com>
2025-12-03 22:43:36 +0530

    Day 3 updates: added new components and fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import HeroCapsule from "./components/HeroCapsule";
+import "./App.css";
+import FlexBoxComponent from "./flex-box/flex-box-component";
+import HelloWorldComponent from "./hello-world/hello-world-component";
+import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
+import PostionComponent from "./positioning/position-component";
+import SymbolOfExcellenceComponent from "./symbol-of-excellence/symbol-of-execllence-component";
+import TrustedbyComponent from "./trusted-by/trusted-by-component";
 
 function App() {
   return (
     <div>
-      <HeroCapsule />
+      {/* <HelloWorldComponent /> */}
+      {/* <FlexBoxComponent /> */}
+      {/* <LeadersTomorrowComponent /> */}
+      {/* <SymbolOfExcellenceComponent /> */}
+      {/* <PostionComponent /> */}
+      <TrustedbyComponent />
     </div>
   );
 }
