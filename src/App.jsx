fdc078022694f184ef89f30dfd3879996580ef51
Sabithra <sabithra777@gmail.com>
2025-12-05 11:24:25 +0530

    Resolved App.jsx stash conflict and restored file

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import "./App.css";
+
+// 🔹 All components (you can enable anytime)
+import FlexBoxComponent from "./flex-box/flex-box-component";
+import HelloWorldComponent from "./hello-world/hello-world-component";
+import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
+import PostionComponent from "./positioning/position-component";
+import SymbolOfExcellenceComponent from "./symbol-of-excellence/symbol-of-execllence-component";
+import TrustedbyComponent from "./trusted-by/trusted-by-component";
+import OurPromise from "./our-promise/our-promise-component";
+import ImpactAtGlance from "./impact-at-glance/impact-at-glance-component";
+
+function App() {
+  return (
+    <div>
+
+      {/*  
+        ============================
+              COMPONENT LIST
+        ============================
+
+        Uncomment any section below when you want to view it.
+      */}
+
+      {/* <HelloWorldComponent /> */}
+      {/* <FlexBoxComponent /> */}
+      {/* <LeadersTomorrowComponent /> */}
+      {/* <SymbolOfExcellenceComponent /> */}
+      {/* <PostionComponent /> */}
+      <TrustedbyComponent /> 
+      {/* <OurPromise /> */}
+
+      {/*  
+        ======================================
+             VISIBLE SECTION RIGHT NOW
+        ======================================
+      */}
+      {/*<ImpactAtGlance />*/}
+
+    </div>
+  );
+}
+
+export default App;
