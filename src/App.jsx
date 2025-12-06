38b810fd8f32b7d8b1996cae274f697b68190734
Sabithra <sabithra777@gmail.com>
2025-12-06 19:24:24 +0530

    Added Day 5 work

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css";
 
 // 🔹 All components (you can enable anytime)
 import FlexBoxComponent from "./flex-box/flex-box-component";
+import FlexWrapComponent from "./flex-wrap/flex-wrap-component";
 import HelloWorldComponent from "./hello-world/hello-world-component";
 import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
+import PositionExampleComponent from "./position-example/position-example-component";
 import PostionComponent from "./positioning/position-component";
 import SymbolOfExcellenceComponent from "./symbol-of-excellence/symbol-of-execllence-component";
 import TrustedbyComponent from "./trusted-by/trusted-by-component";
+import OurPromisesComponent from "./our-promises/our-promises";
 import OurPromise from "./our-promise/our-promise-component";
 import ImpactAtGlance from "./impact-at-glance/impact-at-glance-component";
 
@@ -26,17 +29,19 @@ function App() {
       {/* <FlexBoxComponent /> */}
       {/* <LeadersTomorrowComponent /> */}
       {/* <SymbolOfExcellenceComponent /> */}
+      {/*<PositionExampleComponent />*/}
       {/* <PostionComponent /> */}
       {/* <TrustedbyComponent /> */}
       {/* <OurPromise /> */}
-
+       {/* <FlexWrapComponent /> */} 
+      
+      {/*<ImpactAtGlance />*/}
       {/*  
         ======================================
              VISIBLE SECTION RIGHT NOW
         ======================================
       */}
-      <ImpactAtGlance />
-
+      <OurPromisesComponent />
     </div>
   );
 }
