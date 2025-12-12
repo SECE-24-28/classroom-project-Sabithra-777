41ccf27a9b5001db2534054e35a30e17212184b8
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-12 15:54:15 +0530

    Code is pushed

---
+++ api-function/admin-function.js
@@ -0,0 +1,17 @@
+const admin = require("../models/admin");
+
+exports.getAllRequests = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getDetails = await admin.findById(id).populate("listOfRequest");
+    return res.status(200).json({
+      success: true,
+      data: getDetails.listOfRequest,
+    });
+  } catch (e) {
+    return res.status(404).json({
+      success: false,
+      error: e,
+    });
+  }
+};
