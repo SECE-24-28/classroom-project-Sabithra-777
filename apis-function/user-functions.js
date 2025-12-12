566506223aa6af65ef2e1dc1991891a8f8fe1eac
Sabithra <sabithra777@gmail.com>
2025-12-12 09:15:12 +0530

    Added backend files

--- apis-function/user-functions.js
+++ apis-function/user-functions.js
@@ -1,40 +1,78 @@
 const User = require("../models/user");
 exports.createUser = async (req, res) => {
   try {
-    const { firstName, secondName, email, mobileNumber, password } = req.body;
-    const createUser = await User.create({
-      firstName,
-      secondName,
-      email,
-      mobileNumber,
-      password,
+    const { firstName, lastName, email } = req.body;
+    const creatUser = await User.insertOne({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
     });
-    return res.status(200).json({
+    return res.status(201).json({
       success: true,
       message: "User is created successfully",
-      data: createUser,
     });
   } catch (e) {
-    res.status(400).json({
+    res.status(500).json({
       success: false,
-      error: e.message,
+      error: e,
     });
   }
 };
 
 exports.createManyUsers = async (req, res) => {
   try {
-    const { users } = req.body;
-    const createUsers = await User.insertMany(users);
-    return res.status(200).json({
+    const { details } = req.body;
+    const createUser = await User.insertMany(details);
+    return res.status(201).json({
       success: true,
       message: "Users created successfully",
-      data: createUsers,
     });
   } catch (e) {
-    res.status(400).json({
+    res.status(500).json({
       success: false,
-      error: e.message,
+      error: e,
+    });
+  }
+};
+exports.getAllUsers = async (req, res) => {
+  try {
+    const getDetails = await User.find();
+    // console.log("The users details:", getDetails);
+    return res.status(200).json({
+      success: true,
+      data: getDetails,
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      error: e,
+    });
+  }
+};
+exports.updateEmail = async (req, res) => {
+  try {
+    const {firstName, email } = req.body;
+    const updateResult = await User.updateOne(
+      { firstName: firstName },
+      { $set: { email: email } }
+    );
+    
+    if (updateResult.matchedCount === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    
+    return res.status(200).json({
+      success: true,
+      message: "Email updated successfully",
+      modifiedCount: updateResult.modifiedCount
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      error: e,
     });
   }
 };
