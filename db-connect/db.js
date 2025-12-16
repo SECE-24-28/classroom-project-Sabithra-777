b6b03444780f20b6fe5d3d894306788d3b5bc2f2
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-16 13:55:18 +0530

    Adding mongoose connect

--- db-connect/db.js
+++ db-connect/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 exports.connect = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/LMS-Backend");
+    await mongoose.connect(
+      "mongodb+srv://ak7859437:lUdNj7EzHx6ItZfx@clusterlove.uvn8cgb.mongodb.net/LMS?retryWrites=true&w=majority&appName=ClusterLove"
+    );
     console.log("LMS Db is connected");
   } catch (e) {
     console.log("Error in connecting to the db");
