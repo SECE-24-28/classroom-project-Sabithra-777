a68d13de4d4e1871bbf0ed103b561cb4d4c8ea25
raghuram <raghuram.g2024cse@sece.ac.in>
2025-12-18 09:17:34 +0530

    db.js

--- db-connect/db.js
+++ db-connect/db.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
 exports.connect = async () => {
-  // try {
-  //   await mongoose.connect(
-  //     "mongodb+srv://ak7859437:lUdNj7EzHx6ItZfx@clusterlove.uvn8cgb.mongodb.net/LMS?retryWrites=true&w=majority&appName=ClusterLove"
-  //   );
   try {
-    const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/LMS-Backend';
-    const conn = await mongoose.connect(mongoURI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(
+      "mongodb+srv://ak7859437:lUdNj7EzHx6ItZfx@clusterlove.uvn8cgb.mongodb.net/LMS?retryWrites=true&w=majority&appName=ClusterLove"
+    );
+  
+  // try {
+  //   const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/LMS-Backend';
+  //   const conn = await mongoose.connect(mongoURI);
+  //   console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (e) {
     console.log("Error in connecting to the db");
     console.error(e.message);
